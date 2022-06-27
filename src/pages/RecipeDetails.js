c660d3bbd265b2d5cc8b8ac0ae5bf2339d2c683c
onurbyr <byronurr@gmail.com>
2022-06-27 17:37:14 +0300

    Recipe Details 3

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -13,6 +13,24 @@ const RecipeDetails = () => {
     dispatch(getDetails({ i: id }));
   }, [dispatch]);
 
+  const renderIngredients = () => {
+    var rows = [];
+    for (var i = 1; i <= 20; i++) {
+      let strMeasure = `strMeasure${i}`;
+      let strIngredient = `strIngredient${i}`;
+      recipes.selectedItem[strMeasure] &&
+        recipes.selectedItem[strIngredient] &&
+        rows.push(
+          <li key={i}>
+            {recipes.selectedItem[strMeasure] +
+              " " +
+              recipes.selectedItem[strIngredient]}
+          </li>
+        );
+    }
+    return rows;
+  };
+
   return recipes.selectedItemLoading ? (
     <Spinner color="#212529" height="100vh" />
   ) : (
@@ -20,7 +38,26 @@ const RecipeDetails = () => {
       <div className="reciped-details-image-container">
         <img src={recipes.selectedItem.strMealThumb} alt="" />
       </div>
-      <div>{recipes.selectedItem.strMeal}</div>
+      <div className="recipe-details-header">
+        <h2>{recipes.selectedItem.strMeal}</h2>
+        <div className="recipe-info-container">
+          <div className="recipe-info-line"></div>
+          <div className="recipe-info">
+            <h6>Category: {recipes.selectedItem.strCategory}</h6>
+            <h6>Area: {recipes.selectedItem.strArea}</h6>
+            <h6>Tags: {recipes.selectedItem.strTags}</h6>
+          </div>
+        </div>
+      </div>
+      <div className="instructions-ingredients">
+        <div className="instructions">
+          <p>{recipes.selectedItem.strInstructions}</p>
+        </div>
+        <div className="ingredients">
+          <h4>Ingredients</h4>
+          <ul>{renderIngredients()}</ul>
+        </div>
+      </div>
     </div>
   );
 };
